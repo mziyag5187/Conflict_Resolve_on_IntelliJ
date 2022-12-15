a40ceceba6d0a93975678f4b590810573e13c413
mziya <mziyag5187@gmail.com>
2022-12-15 15:29:45 +0300

    Day-1 work is done

---
+++ src/Day_1.java
@@ -0,0 +1,11 @@
+public class Day_1 {
+    public static void main(String[] args) {
+        System.out.println("Day-1 work");
+        System.out.println("Day-1 work");
+        System.out.println("Day-1 work");
+        System.out.println("Day-1 work");
+        System.out.println("Day-1 work");
+        System.out.println("Day-1 work");
+        System.out.println("Day-1 work");
+    }
+}
